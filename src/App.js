6a58a584c07c0cac6bee20ffa6f34a7e77d066e6
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2021-05-04 16:03:30 +0300

    Thug

--- src/App.js
+++ src/App.js
@@ -46,16 +46,12 @@ class App extends Component {
                 2.0.1
                 <img src={logo} className="App-logo" alt="logo" />
 
-                {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/2tLp-7ZwDz0?autoplay=1&start=28" title="YouTube video player"*/}
-                {/*        frameBorder="0"*/}
-                {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"*/}
-                {/*        allowFullScreen></iframe>*/}
-
-                {/*<h2>What's Up, Thug?</h2>*/}
-
-                {/*fetch('http://jsonplaceholder.typicode.com/users')*/}
-                {/*.then(res => res.json())*/}
-                {/*.then(d => console.log(d));*/}
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/2tLp-7ZwDz0?autoplay=1&start=28" title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                {/**/}
+                <h2>What's Up, Thug?</h2>
 
                 <h1>{this.state.respA}</h1>
                 <h1>{this.state.respAB}</h1>
