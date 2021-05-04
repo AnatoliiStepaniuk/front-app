58324b4ad469afc668e924f10a38ee4b8f0d0f5e
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2021-05-04 14:50:05 +0300

    Fetching data from service-a

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/2tLp-7ZwDz0?autoplay=1&start=28" title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen></iframe>
-          <h2>What's Up, Thug?</h2>
+    constructor(props) {
+        super(props);
+        this.state = {
+            respA: "",
+            respAB: ""
+        }
+    }
 
-      </header>
-    </div>
-  );
+    componentDidMount() {
+
+        // Promise.all([
+        //     fetch('http://localhost:8081/a'),
+        //     fetch('http://localhost:8081/ab')
+        // ])
+        //     .then(([respA, respAB]) => {
+        //         this.setState({
+        //             respA: respA.text(),
+        //             respAB: respAB.text()
+        //         });
+        //     })
+        //     .catch(console.log)
+
+        fetch('http://service-a/a')
+            .then(res => res.text())
+            .then((data) => {
+                this.setState({ respA: data })
+            })
+            .catch(console.log)
+        fetch('http://service-a/ab')
+            .then(res => res.text())
+            .then((data) => {
+                this.setState({ respAB: data })
+            })
+            .catch(console.log)
+    }
+
+    render(){
+        return <div className="App">
+            <header className="App-header">
+                2.0.0
+                <img src={logo} className="App-logo" alt="logo" />
+
+                {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/2tLp-7ZwDz0?autoplay=1&start=28" title="YouTube video player"*/}
+                {/*        frameBorder="0"*/}
+                {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"*/}
+                {/*        allowFullScreen></iframe>*/}
+
+                {/*<h2>What's Up, Thug?</h2>*/}
+
+                {/*fetch('http://jsonplaceholder.typicode.com/users')*/}
+                {/*.then(res => res.json())*/}
+                {/*.then(d => console.log(d));*/}
+
+                <h1>{this.state.respA}</h1>
+                <h1>{this.state.respAB}</h1>
+            </header>
+        </div>
+    }
 }
 
 export default App;
